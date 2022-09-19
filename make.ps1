ef37b70dfe62c4d71fe7b58a6a5795c3a0f56951
LittleboyHarry <littleboyharry@qq.com>
2022-09-19 11:37:34 +0800

    . rename

--- make.ps1
+++ make.ps1
@@ -4,26 +4,28 @@ Remove-Item -Recurse -Force *
 $names = switch ( (Get-WinSystemLocale).Name ) {
     zh-CN {
         @{
-            enabledarkmode  = '激活 - 深色模式';
-            disabledarkmode = '关闭 - 深色模式';
-            edithosts       = '编辑 - HOSTS';
-            editdesktopico  = '调整桌面图标';
-            flushdns        = '重置 DNS 解析';
-            restartexp      = '重启文件资源管理器';
-            enablehyperv    = '激活 - HyperV（重启后生效）';
-            disablehyperv   = '关闭 - HyperV（重启后生效）';
+            desc_reboot      = '重启后生效';
+            enable_darkmode  = '激活 - 深色模式';
+            disable_darkmode = '关闭 - 深色模式';
+            edit_hosts       = '编辑 - HOSTS';
+            edit_desktopicon = '调整桌面图标';
+            flushdns         = '重置 DNS 解析';
+            restartexp       = '重启文件资源管理器';
+            enable_hyperv    = '激活 - HyperV';
+            disable_hyperv   = '关闭 - HyperV';
         }
     }
     default {
         @{
-            enabledarkmode  = 'Enable - Dark Mode';
-            disabledarkmode = 'Disable - Dark Mode';
-            edithosts       = 'Edit - HOSTS';
-            editdesktopico  = 'Edit - Desktop Icon';
-            flushdns        = 'flush DNS resolve'
-            restartexp      = 'Restart Explorer';
-            enablehyperv    = 'Enable - HyperV (After Restart)';
-            disablehyperv   = 'Disable - HyperV (After Restart)';
+            desc_reboot      = 'Reboot to Apply';
+            enable_darkmode  = 'Enable - Dark Mode';
+            disable_darkmode = 'Disable - Dark Mode';
+            edit_hosts       = 'Edit - HOSTS';
+            edit_desktopicon = 'Edit - Desktop Icon';
+            flushdns         = 'flush DNS resolve'
+            restartexp       = 'Restart Explorer';
+            enable_hyperv    = 'Enable - HyperV';
+            disable_hyperv   = 'Disable - HyperV';
         }
     }
 }
@@ -62,19 +64,19 @@ function Set-ShortcutEditIcon {
     $shortcut.IconLocation = "shell32.dll,269"
 }
 
-$it = New-Shortcut $names.enabledarkmode
+$it = New-Shortcut $names.enable_darkmode
 $it.TargetPath = "reg"
 $it.Arguments = "add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d 0 /f"
 Set-ShortcutEnableIcon $it
 $it.Save()
 
-$it = New-Shortcut $names.disabledarkmode
+$it = New-Shortcut $names.disable_darkmode
 $it.TargetPath = "reg"
 $it.Arguments = "add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d 1 /f"
 Set-ShortcutDisableIcon $it
 $it.Save()
 
-$it = New-Shortcut $names.edithosts
+$it = New-Shortcut $names.edit_hosts
 $it.TargetPath = "notepad"
 $it.Arguments = "C:\Windows\system32\drivers\etc\hosts"
 Set-ShortcutEditIcon $it
@@ -94,21 +96,23 @@ $it.Arguments = "-c kill -n explorer"
 Set-ShortcutRestartIcon $it
 $it.Save()
 
-$it = New-Shortcut $names.editdesktopico
+$it = New-Shortcut $names.edit_desktopicon
 $it.TargetPath = "control"
 $it.Arguments = "desk.cpl,,0"
 $it.Save()
 
-$it = New-Shortcut $names.enablehyperv
+$it = New-Shortcut $names.enable_hyperv
 $it.TargetPath = "bcdedit"
 $it.Arguments = "/set {current} hypervisorlaunchtype auto"
+$it.Description = $names.desc_reboot
 Set-ShortcutEnableIcon $it
 $it.Save()
 Set-ShortcutRequireAdmin $it
 
-$it = New-Shortcut $names.disablehyperv
+$it = New-Shortcut $names.disable_hyperv
 $it.TargetPath = "bcdedit"
 $it.Arguments = "/set {current} hypervisorlaunchtype off"
+$it.Description = $names.desc_reboot
 Set-ShortcutDisableIcon $it
 $it.Save()
 Set-ShortcutRequireAdmin $it
