b42ad35baffa521e03eda30564d4739da7fd47c6
LittleboyHarry <littleboyharry@qq.com>
2022-09-21 12:15:01 +0800

    + win11 context menu

--- make.ps1
+++ make.ps1
@@ -4,30 +4,32 @@ Remove-Item -Recurse -Force *
 $names = switch ( (Get-WinSystemLocale).Name ) {
     zh-CN {
         @{
-            desc_reboot      = '重启后生效';
-            enable_darkmode  = '激活 - 深色模式';
-            disable_darkmode = '关闭 - 深色模式';
-            edit_hosts       = '编辑 - HOSTS';
-            edit_desktopicon = '调整桌面图标';
-            flushdns         = '重置 DNS 解析';
-            restartexp       = '重启文件资源管理器';
-            enable_hyperv    = '激活 - HyperV';
-            disable_hyperv   = '关闭 - HyperV';
-            clear_histpwsh   = '清除 - PowerShell 历史记录';
+            enable_darkmode    = '激活 - 深色模式';
+            disable_darkmode   = '禁用 - 深色模式';
+            edit_hosts         = '编辑 - HOSTS';
+            edit_desktopicon   = '调整桌面图标';
+            flushdns           = '重置 DNS 解析';
+            restartexp         = '重启文件资源管理器';
+            enable_hyperv      = '激活 - HyperV (需重启)';
+            disable_hyperv     = '禁用 - HyperV (需重启)';
+            clear_histpwsh     = '清除 - PowerShell 历史记录';
+            enable_w11ctxmenu  = '激活 - 新风格菜单 (需注销)';
+            disable_w11ctxmenu = '禁用 - 新风格菜单 (需注销)';
         }
     }
     default {
         @{
-            desc_reboot      = 'Reboot to Apply';
-            enable_darkmode  = 'Enable - Dark Mode';
-            disable_darkmode = 'Disable - Dark Mode';
-            edit_hosts       = 'Edit - HOSTS';
-            edit_desktopicon = 'Edit - Desktop Icon';
-            flushdns         = 'flush DNS resolve'
-            restartexp       = 'Restart Explorer';
-            enable_hyperv    = 'Enable - HyperV';
-            disable_hyperv   = 'Disable - HyperV';
-            clear_histpwsh   = 'Clear - History of PowerShell';
+            enable_darkmode    = 'Enable - Dark Mode';
+            disable_darkmode   = 'Disable - Dark Mode';
+            edit_hosts         = 'Edit - HOSTS';
+            edit_desktopicon   = 'Edit - Desktop Icon';
+            flushdns           = 'flush DNS resolve'
+            restartexp         = 'Restart Explorer';
+            enable_hyperv      = 'Enable - HyperV (need Reboot)';
+            disable_hyperv     = 'Disable - HyperV (need Reboot)';
+            clear_histpwsh     = 'Clear - History of PowerShell';
+            enable_w11ctxmenu  = 'Enable - New Design Context Menu (need Relogin)';
+            disable_w11ctxmenu = 'Disable - New Design Context Menu (need Relogin)';
         }
     }
 }
@@ -113,7 +115,6 @@ $it.Save()
 $it = New-Shortcut $names.enable_hyperv
 $it.TargetPath = "bcdedit"
 $it.Arguments = "/set {current} hypervisorlaunchtype auto"
-$it.Description = $names.desc_reboot
 Set-ShortcutEnableIcon $it
 $it.Save()
 Set-ShortcutRequireAdmin $it
@@ -121,7 +122,6 @@ Set-ShortcutRequireAdmin $it
 $it = New-Shortcut $names.disable_hyperv
 $it.TargetPath = "bcdedit"
 $it.Arguments = "/set {current} hypervisorlaunchtype off"
-$it.Description = $names.desc_reboot
 Set-ShortcutDisableIcon $it
 $it.Save()
 Set-ShortcutRequireAdmin $it
@@ -131,3 +131,17 @@ $it.TargetPath = "cmd"
 $it.Arguments = "/c del %APPDATA%\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
 Set-ShortcutDeleteFileIcon $it
 $it.Save()
+
+if ($iswin11) {
+    $it = New-Shortcut $names.enable_w11ctxmenu
+    $it.TargetPath = "reg"
+    $it.Arguments = "add HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32 /f /ve"
+    Set-ShortcutEnableIcon $it
+    $it.Save()
+
+    $it = New-Shortcut $names.disable_w11ctxmenu
+    $it.TargetPath = "reg"
+    $it.Arguments = "delete HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32 /f /ve"
+    Set-ShortcutDisableIcon $it
+    $it.Save()
+}
